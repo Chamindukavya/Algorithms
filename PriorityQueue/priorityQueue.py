e9a575915d53a5a9dc040ceed20c797b317ca6e4
Gawasingha Arachchige Chamindu Kavya <162815316+Chamindukavya@users.noreply.github.com>
2024-12-23 12:37:42 +0530

    heap sort

--- PriorityQueue/priorityQueue.py
+++ PriorityQueue/priorityQueue.py
@@ -1,34 +1,44 @@
-data = [16,14,8,10,1,3,9,2,4,7]
+data = [8,7,6,5,4,3,2,1]
 
 def maxHeapify(A,i):
     l = (i*2)+1
     r = (i*2)+2
 
-    largest = i
-
     if(l<=(len(A)-1) and A[l]>A[i]):
         largest = l
-    elif(r<=(len(A)-1) and A[r]>A[i]):
+    else:
+        largest = i
+
+    if(r<=(len(A)-1) and A[r]>A[largest]):
         largest = r
-    ##print("largest" , largest)
     if(largest != i):
-        print("A[L]",A[l])
-        ##print("A[r]",A[r])
         A[largest],A[i] = A[i],A[largest]
-        ##print("on road ", A)
         maxHeapify(A,largest)
-
-
-
-
 
 def buildHeap(A):
     heapSize = len(A) - 1
     startIndex = (heapSize-1)//2
-    ##print(startIndex)
     for i in range(startIndex,-1,-1):
-        ##print(i)
         maxHeapify(A,i)
-    print(A)
 
-buildHeap(data)
+def heapSort(A):
+
+    if (len(A) == 0):
+        return
+    if (len(A) == 1):
+        sortedArray.append(A[0])
+        return
+
+    buildHeap(A)
+    arraySize = len(A)
+    while(arraySize > 0):
+        A[arraySize-1],A[0] = A[0],A[arraySize-1]
+        sortedArray[arraySize-1] = A[arraySize-1]
+        arraySize = arraySize -1
+        A.pop()
+        maxHeapify(A,0)
+
+sortedArray = [0]*len(data)
+heapSort(data)
+print(sortedArray)
+
