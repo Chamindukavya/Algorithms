5626009769a25a20291abf65045a6b033f5df25a
Gawasingha Arachchige Chamindu Kavya <162815316+Chamindukavya@users.noreply.github.com>
2024-12-24 12:08:00 +0530

    dijkstras sketch

---
+++ Dijkstra's Algorithm/findShortestPath.py
@@ -0,0 +1,30 @@
+class Graph:
+
+    def __init__(self,numberOfVertices):
+        
+        self.V = numberOfVertices
+        self.g = []
+        for i in range(self.V):
+            self.g.append([0]*self.V)
+
+    def dijkstra():
+        pass        
+
+        
+
+if __name__ == "__main__":
+    
+    graph = Graph(9)
+
+    graph.g = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+               [4, 0, 8, 0, 0, 0, 0, 11, 0],
+               [0, 8, 0, 7, 0, 4, 0, 0, 2],
+               [0, 0, 7, 0, 9, 14, 0, 0, 0],
+               [0, 0, 0, 9, 0, 10, 0, 0, 0],
+               [0, 0, 4, 14, 10, 0, 2, 0, 0],
+               [0, 0, 0, 0, 0, 2, 0, 1, 6],
+               [8, 11, 0, 0, 0, 0, 1, 0, 7],
+               [0, 0, 2, 0, 0, 0, 6, 7, 0]
+               ]
+    
+   
